da5fbdf8107501670e5a09e3a5572f1b9c697d72
shraddesh123 <shraddheshdeshmukh23@gmail.com>
2024-08-10 11:31:36 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Footer from './Footer'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import SpecialOffers from './SpecialOffers'
+import CustomerReviews from './CustomerReviews'
+export {
+    Hero,
+    PopularProducts,
+    Footer,
+    Services,
+    Subscribe,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+}
